9789c3a22ce9dc1133fe2e401cac1e1cac469ea8
Junya Oiwa <0920oiwa@gmail.com>
2020-07-13 00:51:05 +0900

    [add]ログインUIを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 
+import { FirebaseContext } from "./Firebase";
+import { FirebaseAuth, signInWithRedirect, signOut } from "./FirebaseAuth";
+
+const Content: React.FC = () => {
+  const { userId, userName } = useContext(FirebaseContext);
+  return (
+    <div>
+      {userName} ({userId}) がログイン中
+    </div>
+  );
+};
 
 const App: React.FC = () => {
-  return <div>test</div>;
+  const NotSignedIn = React.useCallback(() => {
+    return <button onClick={() => signInWithRedirect()}>ログイン</button>;
+  }, []);
+
+  const Loading = React.useCallback(() => {
+    return <div>読み込み中...</div>;
+  }, []);
+
+  return (
+    <FirebaseAuth NotSignedIn={NotSignedIn} Loading={Loading}>
+      <Content />
+      <button onClick={signOut}>ログアウト</button>
+    </FirebaseAuth>
+  );
 };
 
 export default App;
